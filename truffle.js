738b05f41eacae75d3e69c57e1c801e4b0a44564
jann7790 <jann7790@gmail.com>
2022-02-08 20:24:20 +0800

    first init

--- truffle.js
+++ truffle.js
@@ -1,42 +1,28 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const fs = require('fs')
-
-// First read in the secrets.json to get our mnemonic
-let secrets
-let mnemonic
-if (fs.existsSync('secrets.json')) {
-  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
-  mnemonic = secrets.mnemonic
-} else {
-  console.log('No secrets.json found. If you are trying to publish EPM ' +
-              'this will fail. Otherwise, you can ignore this message!')
-  mnemonic = ''
-}
-
 module.exports = {
   networks: {
-    live: {
-      network_id: 1 // Ethereum public network
-      // optional config values
-      // host - defaults to "localhost"
-      // port - defaults to 8545
-      // gas
-      // gasPrice
-      // from - default address to use for any transaction Truffle makes during migrations
-    },
-    ropsten: {
-      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
-      network_id: '3'
-    },
-    testrpc: {
-      network_id: 'default'
-    },
-    coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,        
-      gas: 0xfffffffffff,
-      gasPrice: 0x01     
-    },
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.21", // A version or constraint - Ex. "^0.5.0"
+                         // Can be set to "native" to use a native solc or
+                         // "pragma" which attempts to autodetect compiler versions
+      docker: 0, // Use a version obtained through docker
+      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: 1,
+          runs: 1   // Optimize for how many times you intend to run the code
+        },
+        evmVersion:  "byzantium"// Default: "istanbul"
+      },
+      modelCheckerSettings: {
+        // contains options for SMTChecker
+      }
+    }
   }
-}
+};
